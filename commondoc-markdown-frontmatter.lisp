267a6140c2a3c880d767dfe46b91b2e0a73842fe
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-02-11 08:29:21 +0100

    fix: Use imported symbols

--- commondoc-markdown-frontmatter.lisp
+++ commondoc-markdown-frontmatter.lisp
@@ -3,15 +3,16 @@
   (:import-from :commondoc-markdown
                 :markdown)
   (:import-from :common-doc
-                :parse-document
                 :metadata)
+  (:import-from :common-doc.format
+                :parse-document)
   (:export :markdown-frontmatter))
 
 (in-package :commondoc-markdown-frontmatter)
 
-(defclass markdown-frontmatter (commondoc-markdown:markdown) ())
+(defclass markdown-frontmatter (markdown) ())
 
-(defmethod common-doc.format:parse-document :around ((markdown markdown-frontmatter) (input string))
+(defmethod parse-document :around ((markdown markdown-frontmatter) (input string))
   "Parse yaml frontmatter and add it to the metadata of the returned node"
   (if (string= (subseq input 0 3) "---")
       (let* ((yaml-start-position (1+ (search (string #\Newline) input)))
@@ -20,6 +21,6 @@
              (yaml-string (subseq input yaml-start-position yaml-end-position))
              (yaml (yaml:parse yaml-string))
              (parsed-document (call-next-method markdown (subseq input (+ 3 yaml-end-position)))))
-        (setf (common-doc:metadata parsed-document) yaml)
+        (setf (metadata parsed-document) yaml)
         parsed-document)
       (call-next-method)))
